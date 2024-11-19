e97076dd11fb038432b0580a85dfa4531eaccd46
Arshadalee <arshadalimulla9@gmail.com>
2024-11-19 15:33:12 +0530

    dark mode added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Accordian from "./comp/accordian/Accordian";
 import StarRating from "./comp/starRating/StarRating";
 import Slider from "./comp/image-slider/Slider";
 import LoadMore from "./comp/Loading/LoadMore";
+import DarkMod from "./comp/DarkMod/DarkMod";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello this is my new project</h2>
+      <DarkMod/>
       <Accordian />
       <StarRating />
       <Slider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
---
+++ src/comp/DarkMod/DarkMod.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import useLocalStorage from './useLocalStorage';
+import  "./Style.css";
+
+
+function DarkMod() {
+    const[theme,setTheme]=useLocalStorage('theme','dark')
+
+    function handleTheme(){
+        setTheme(theme==='light'?'dark':'light')
+    }
+    console.log(theme,"this is the theme testing")
+  return (
+   <div className='light-dark-mode' data-theme={theme}>
+    <center> <div className="container">
+    <p>{theme} Mode</p>
+    <button onClick={handleTheme} >Change Theme</button>
+      
+    </div></center>
+   </div>
+  )
+}
+
+export default DarkMod
